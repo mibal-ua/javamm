e55767e514e0508b45b09a79d95f0f6c7a92f2c7
mibal_ua <9mohapx9@gmail.com>
2022-11-05 16:56:23 +0100

    Add exports to code fragment info

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -3,5 +3,7 @@
  * @link t.me/mibal_ua.
  */
 module javamm.code {
-
+    exports ua.mibal.javamm.code.fragment;
+    exports ua.mibal.javamm.code.fragment.operation;
+    exports ua.mibal.javamm.code.exception;
 }
