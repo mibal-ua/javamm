4ace42c20f8f01a2b73f19fc2fdee07b4b8b88ba
mibal_ua <9mohapx9@gmail.com>
2023-01-17 17:31:56 +0100

    Implement TokenParserResult class

--- compiler/src/main/java/ua/mibal/javamm/compiler/model/TokenParserResult.java
+++ compiler/src/main/java/ua/mibal/javamm/compiler/model/TokenParserResult.java
@@ -17,10 +17,36 @@
 
 package ua.mibal.javamm.compiler.model;
 
+import java.util.List;
+
 /**
  * @author Michael Balakhon
  * @link t.me/mibal_ua.
  */
-public class TokenParserResult {
+public final class TokenParserResult {
 
+    private final List<String> tokens;
+
+    private final boolean multilineCommentStarted;
+
+    public TokenParserResult(final List<String> tokens, final boolean multilineCommentStarted) {
+        this.tokens = List.copyOf(tokens);
+        this.multilineCommentStarted = multilineCommentStarted;
+    }
+
+    public TokenParserResult(final boolean multilineCommentStarted) {
+        this(List.of(), multilineCommentStarted);
+    }
+
+    public List<String> getTokens() {
+        return tokens;
+    }
+
+    public boolean isNotEmpty() {
+        return !tokens.isEmpty();
+    }
+
+    public boolean isMultilineCommentStarted() {
+        return multilineCommentStarted;
+    }
 }
