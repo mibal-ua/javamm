1d59ff696c71d5c8f7b5852f7056f6696433b597
mibal_ua <9mohapx9@gmail.com>
2023-01-17 15:21:52 +0100

    Rearrange code

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -4,6 +4,6 @@
  */
 module javamm.code {
     exports ua.mibal.javamm.code.fragment;
-    exports ua.mibal.javamm.code.fragment.operation;
     exports ua.mibal.javamm.code.exception;
+    exports ua.mibal.javamm.code.fragment.operation;
 }
