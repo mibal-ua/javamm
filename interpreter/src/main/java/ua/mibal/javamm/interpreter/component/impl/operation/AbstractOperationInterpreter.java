2f822be10c9e8d6868eb0ce03a078e9be9503e6d
mibal_ua <9mohapx9@gmail.com>
2023-01-31 17:04:08 +0100

    Realize AbstractOperationInterpreter

---
+++ interpreter/src/main/java/ua/mibal/javamm/interpreter/component/impl/operation/AbstractOperationInterpreter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ua.mibal.javamm.interpreter.component.impl.operation;
+
+import ua.mibal.javamm.code.fragment.Operation;
+import ua.mibal.javamm.interpreter.TerminateInterpreterException;
+import ua.mibal.javamm.interpreter.component.OperationInterpreter;
+
+/**
+ * @author Michael Balakhon
+ * @link t.me/mibal_ua.
+ */
+public abstract class AbstractOperationInterpreter<T extends Operation> implements OperationInterpreter<T> {
+
+    @Override
+    public final void interpret(final T operation) {
+        checkForTerminate();
+        interpretOperation(operation);
+    }
+
+    protected final void checkForTerminate() {
+        if (Thread.interrupted()) {
+            throw new TerminateInterpreterException();
+        }
+    }
+
+    protected abstract void interpretOperation(T operation);
+}
