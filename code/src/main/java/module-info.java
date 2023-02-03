8dceb745f65dc1ae3489b4bc6a951e54f0650dca
mibal_ua <9mohapx9@gmail.com>
2023-02-03 08:46:14 +0100

    Exports expressions and keywords syntax

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -6,4 +6,7 @@ module javamm.code {
     exports ua.mibal.javamm.code.fragment;
     exports ua.mibal.javamm.code.exception;
     exports ua.mibal.javamm.code.fragment.operation;
+    exports ua.mibal.javamm.code.fragment.expression;
+    exports ua.mibal.javamm.code.syntax;
+    exports ua.mibal.javamm.code.component;
 }
